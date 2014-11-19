84c00cd5ddd7bc1db2bc5241c8f047af9936464b
George Pickering <george_pickering@yahoo.com>
2014-11-19 12:46:40 +0000

    George and tyler are starting a chatbot

---
+++ GeoTyChatBot.java
@@ -0,0 +1,37 @@
+/** 
+ * George and tyler created this simple chat bot,
+ * in hopes that one day it will grow to be something
+ * really big. Or until we start building games.
+ * 
+ * A simple code that will use a slew of else if statements
+ * to set a response of the chatbot. Scanner will
+ * be located in the tester. Have fun!
+ * 
+ */
+ 
+ public class GeoTyChatBot{
+     
+     /**
+      * Get greeting of the bot
+      */
+     
+     public String getIntro(){
+         return "Hi there! You seem bored, let's talk!";
+     }
+     
+     /**
+      * Set statements(responses) for various keywords
+      * 
+      * @param statement The sentence input by the user
+      * @return A response based on the present keywords
+      */
+      
+      public String getResponse(String statement) {
+        String response="";
+        if(statement.indexOf(" no ")>=0{
+            response="Aw, don't be so negative!";
+        }
+        else if(statement.indexOf(" mother ") >=0
+     
+ }
+    
