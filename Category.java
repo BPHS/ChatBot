f746045add63d7b0073d32b1f7e781f6af501d0f
Tyler Schmidt <15schmidtt@gapps.bpcsd.org>
2014-12-05 14:30:55 +0000

    The Category file; not finished

---
+++ Category.java
@@ -0,0 +1,24 @@
+public class Category {
+
+    private Questions[] questions;        
+        
+    public Category(String questions_file, String answers_file) {
+        
+        // open both files for reading
+        
+        // create questions array with size of total rows in file
+        
+        // read each file sychronously
+        
+        // put each row into the questions arrray
+        
+        
+    }
+        
+        
+        
+        
+        
+        
+    }
+}
