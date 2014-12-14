949d8ef1725b471ba57c31c2e85e52f7ce986ef9
George Pickering <george_pickering@yahoo.com>
2014-12-14 14:57:49 +0000

    Updated Questions.java class. Now able to open movie questions text file, read lines, and randomly select a line to be printed to screen. Now we have to match answer to the question line randomly selected. Cheers

--- Questions.java
+++ Questions.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.io.IOException;
 
 public class Questions{
     
@@ -8,17 +9,44 @@ public class Questions{
         private String correctAnswer;
         private String userAnswer;
         */
-        java.io.File MovieQuestionsText=new java.io.File("TriviaBotMovieQuestions.txt");
+        /*java.io.File MovieQuestionsText=new java.io.File("TriviaBotMovieQuestions.txt");
     
         try{
             Scanner input=new Scanner(MovieQuestionsText);
             while(input.hasNext()){
-                String num=input.nextLine();
-                System.out.println(num);
-            }
+                int questionsReadIn=input.nextLine();
+                int movieQuestionPicker[]=new int[questionsReadIn];
+                for(i=0;i<movieQuestionPicker.length; i++){
+                    
+                }
+                    
+                }
+                
         }
         catch(FileNotFoundException e){
             System.err.format("error. file not found");
         }
+        */
+        // Read in the file into a list of strings
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("TriviaBotMovieQuestions.txt"));
+            List<String> lines = new ArrayList<String>();
+
+            String line = reader.readLine();
+
+            while( line != null ) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+
+            // Choose a random one from the list
+            Random r = new Random();
+            String randomString = lines.get(r.nextInt(lines.size()));
+            System.out.println(randomString);
+        }
+        catch(IOException ioe){
+            System.out.println(ioe.toString());
+        }
+            
     }
 }
